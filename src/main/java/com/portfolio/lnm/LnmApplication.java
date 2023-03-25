44ba3254d6af11e2359d6856c661a1e8bf398f5d
LeandroMoyano <108894094+LeandroMoyano@users.noreply.github.com>
2023-03-25 13:22:46 -0300

    Initial commit

---
+++ src/main/java/com/portfolio/lnm/LnmApplication.java
@@ -0,0 +1,14 @@
+package com.portfolio.lnm;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LnmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LnmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/lnm/LnmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.lnm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LnmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
